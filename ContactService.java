709707a1190b93af65aaf30ce64b192716db5520
Lewis Quick <82186284+lew-pop@users.noreply.github.com>
2024-07-19 16:22:53 -0700

    Update ContactService.java

--- ContactService.java
+++ ContactService.java
@@ -2,18 +2,20 @@ import java.util.HashMap;
 import java.util.function.BiFunction;
 
 public class ContactService {
-
+    // Data structure to store contacts
     HashMap<String, Contact> contactList;
-
+    
+    // Initialize Contact Service with empty contact list.
     public ContactService() {
         contactList = new HashMap<String, Contact>();
     }
 
+    // Function to get an instance of the contact service.
      public HashMap<String, Contact> getContactService() {
         return contactList;
 
     }
-
+    // Contact service to retrieve a contact from the contact list.
     public Contact getContact(String contactId) {
         if(contactList.containsKey(contactId)){
             return contactList.get(contactId);
@@ -22,7 +24,7 @@ public class ContactService {
             throw new NullPointerException("Contact ID does not exist");
         }
     }
-
+     // Contact service to add a contact to the contact list.
     public void addContact(String contactId, Contact contact) {
         if(contactList.containsKey(contactId)){
             throw new IllegalArgumentException("Contact ID already exists");
@@ -31,7 +33,7 @@ public class ContactService {
             contactList.put(contactId, contact);
         }
     }
-
+     // Contact service to remove a contact from the contact list.
     public void removeContact(String contactId) {
         if(contactList.containsKey(contactId)){
             contactList.remove(contactId);
@@ -40,7 +42,7 @@ public class ContactService {
             throw new IllegalArgumentException("Contact ID does not exist");
         }
     }
-
+     // Contact service to modify a contact's first name.
     public void updateFirstName(String contactId, String newFirstName) {
         Contact tempContact = contactList.get(contactId);
         tempContact.setFirstName(newFirstName);
@@ -48,7 +50,7 @@ public class ContactService {
         contactList.put(contactId, tempContact);
 
     }
-
+     // Contact service to modify a contact's last name.
     public void updateLastName(String contactId, String newLastName) {
 
         Contact tempContact = contactList.get(contactId);
@@ -57,7 +59,7 @@ public class ContactService {
         contactList.put(contactId, tempContact);
 
     }
-
+     // Contact service to modify a contact's phone number.
     public void updatePhoneNumber(String contactId, String phoneNumber) {
         Contact tempContact = contactList.get(contactId);
         tempContact.setPhoneNumber(phoneNumber);
@@ -65,7 +67,7 @@ public class ContactService {
         contactList.put(contactId, tempContact);
 
     }
-
+     // Contact service to modify a contact's address.
     public void updateAddress(String contactId, String newAddress) {
         Contact tempContact = contactList.get(contactId);
         tempContact.setContactAddress(newAddress);
